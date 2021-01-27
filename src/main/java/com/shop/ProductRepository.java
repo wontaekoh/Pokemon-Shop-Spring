cfec044164b4529dc2d4b9e1addbe155420a583c
wontaekoh <wontaek.oh.5@gmail.com>
2021-01-27 18:14:42 -0500

    Added header template

--- src/main/java/com/shop/ProductRepository.java
+++ src/main/java/com/shop/ProductRepository.java
@@ -4,6 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Products, Integer> {
     
-//    @Query("SELECT p FROM Products p WHERE p.id = ?1")
-//    public Optional<Products> findById(Integer id);
 }
